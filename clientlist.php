fb5e2919229a097980a9a47549cfff779fa4bbc8
zakirhussainb <zakirhussainb2693@gmail.com>
2018-04-06 15:43:29 +0530

    clientlist

--- clientlist.php
+++ clientlist.php
@@ -1,7 +1,7 @@
 
 <div class="box box-primary">
 <div class="box-header with-border">
-  <h3 class="box-title">Our Client List</h3>
+  <h3 class="box-title">Our Client List for all clients</h3>
 </div>
 
 <div class="box-body">
